84a513fe238a63ac5a2e67c33dcc88ae29cffe14
Kyoto321 <bankoleayodele147@gmail.com>
2022-09-03 00:02:20 +0100

    BlogForum

--- ablog/settings.py
+++ ablog/settings.py
@@ -150,4 +150,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
